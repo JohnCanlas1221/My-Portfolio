00239879cdd8ea8b1cd0c4cd512a8df5b768fede
John Francis Canlas <personal.johncanlas@gmail.com>
2025-06-28 16:16:32 +0800

    changes

--- src/components/techskills.jsx
+++ src/components/techskills.jsx
@@ -6,16 +6,16 @@ function techskills() {
         <div className='m-auto mt-7 mb-7 justify-center'>
             <h3 className='text-white text-center font-semibold text-2xl -tracking-wider'>TECHNICAL SKILLS</h3>
             <div className='flex justify-center align-center p-7 gap-10'>
-                <img src="./src/assets/images/html.png" className='w-auto h-9 hover:scale-120 duration-200 ease-in-out' />
-                <img src="./src/assets/images/css.png" className='w-auto h-9 hover:scale-120 duration-200 ease-in-out' />
-                <img src="./src/assets/images/javascript.png" className='w-auto h-9 hover:scale-120 duration-200 ease-in-out' />
-                <img src="./src/assets/images/react.png" className='w-auto h-9 hover:scale-120 duration-200 ease-in-out' />
-                <img src="./src/assets/images/tailwindcss.png" className='w-auto h-9 hover:scale-120 duration-200 ease-in-out' />
-                <img src="./src/assets/images/figma.png" className='w-auto h-9 hover:scale-120 duration-200 ease-in-out' />
-                <img src="./src/assets/images/mysql.png" className='w-auto h-9 hover:scale-120 duration-200 ease-in-out' />
-                <img src="./src/assets/images/git.png" className='w-auto h-9 hover:scale-120 duration-200 ease-in-out' />
-                <img src="./src/assets/images/visualstudio.png" className='w-auto h-9 hover:scale-120 duration-200 ease-in-out' />
-                <img src="./src/assets/images/vscode.png" className='w-auto h-9 hover:scale-120 duration-200 ease-in-out' />
+                <img src="./src/assets/images/html.png" className='w-auto h-9 hover:scale-130 duration-400 ease-in-out' />
+                <img src="./src/assets/images/css.png" className='w-auto h-9 hover:scale-130 duration-400 ease-in-out' />
+                <img src="./src/assets/images/javascript.png" className='w-auto h-9 hover:scale-130 duration-400 ease-in-out' />
+                <img src="./src/assets/images/react.png" className='w-auto h-9 hover:scale-130 duration-400 ease-in-out' />
+                <img src="./src/assets/images/tailwindcss.png" className='w-auto h-9 hover:scale-130 duration-400 ease-in-out' />
+                <img src="./src/assets/images/figma.png" className='w-auto h-9 hover:scale-130 duration-400 ease-in-out' />
+                <img src="./src/assets/images/mysql.png" className='w-auto h-9 hover:scale-130 duration-400 ease-in-out' />
+                <img src="./src/assets/images/git.png" className='w-auto h-9 hover:scale-130 duration-400 ease-in-out' />
+                <img src="./src/assets/images/visualstudio.png" className='w-auto h-9 hover:scale-130 duration-400 ease-in-out' />
+                <img src="./src/assets/images/vscode.png" className='w-auto h-9 hover:scale-130 duration-400 ease-in-out' />
 
             </div>
         </div>
