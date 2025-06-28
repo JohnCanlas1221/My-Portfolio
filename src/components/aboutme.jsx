290a5904c02fefb9cc17241685d322163b78f66c
John Francis Canlas <personal.johncanlas@gmail.com>
2025-06-28 16:13:54 +0800

    changes

--- src/components/aboutme.jsx
+++ src/components/aboutme.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-function aboutme({ title, description }) {
+function aboutme({ title, description, image}) {
 
   return (
     <>
       <div className=''>
-
-        <h3 className='text-white'>{title}</h3>
+        <img src={image} className='w-auto h-15 m-auto mb-3 border-2 border-solid border-cyan-500 p-2 rounded-[50%] '></img> 
+        <h3 className='text-indigo-700 font-semibold mb-3'>{title}</h3>
         <p className='text-white'>{description}</p>
       </div>
     </>
