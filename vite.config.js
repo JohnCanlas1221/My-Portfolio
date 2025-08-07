b155c26fcedcece72096d54b597c2741033cf8f0
John Francis Canlas <personal.johncanlas@gmail.com>
2025-08-08 03:42:06 +0800

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
 import { defineConfig } from 'vite'
-import path from 'path'; // ✅ Add this line
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
-  base: "/", 
-    resolve: {
-    alias: {
-      src: path.resolve(__dirname, './src'),
-    },
-  },
 })
